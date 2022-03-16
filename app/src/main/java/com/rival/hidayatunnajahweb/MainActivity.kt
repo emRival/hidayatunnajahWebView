f4766383f10379a1a1713a9efb3f45680cc3a015
emRival <em.rival282@gmail.com>
2022-03-16 20:21:39 +0700

    first commit

---
+++ app/src/main/java/com/rival/hidayatunnajahweb/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.rival.hidayatunnajahweb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    // deklarasikan webview
+    lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // menghilangkan header dan statusbar
+        supportActionBar?.hide()
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+
+        webView = findViewById(R.id.WV)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.hidayatunnajah.or.id/")
+
+        // web setting
+        val webSettings = webView.settings
+
+        // mengaktifkan javascript
+        webSettings.javaScriptEnabled = true
+
+        // mengaktifkan tool seperti bootstrap
+        webSettings.domStorageEnabled = true
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+
+
+    }
+}
